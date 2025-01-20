c958f677b0fcbcdf61f5412eaed7622e7d785b18
hammadurrehman01 <hammadurrehman1954@gmail.com>
2025-01-20 16:02:58 +0500

    feat: add patient form with validation and new UI components

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import PatientForm from "@/components/forms/PatientForm";
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
-
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
-      {/* TODO: OTP Verfication | Passkey Modal */}
+      {/* TODO OTP Verfication | Passkey Modal */}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,6 +1,24 @@
-import React from "react";
+"use client";
+
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+});
 
 const PatientForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
   return <div>Patient Form</div>;
 };
 
