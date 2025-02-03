948a8568d201ce438fb5247e0bf48abe8e3df9d2
hammadurrehman01 <hammadurrehman1954@gmail.com>
2025-02-03 16:48:58 +0500

    feat: add Appwrite configuration and update language in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default async function RootLayout({
 }>) {
 
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="urd" suppressHydrationWarning>
       <body
         className={cn(
           "min-h-screen bg-dark-300 font-sans antialiased",
